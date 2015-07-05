0d747e0ab0de38f56ef3ea1420cb80e5faa7f000
Waldo Menkveld <wrmenkveld@gmail.com>
2015-07-05 13:29:54 +0200

    Added autoupdate component

---
+++ ZAProxy.Tests/Components/AutoUpdateTests.cs
@@ -0,0 +1,437 @@
+﻿using FluentAssertions;
+using Moq;
+using Newtonsoft.Json.Linq;
+using Ploeh.AutoFixture.Xunit2;
+using Xunit;
+using ZAProxy.Components;
+using ZAProxy.Infrastructure;
+using ZAProxy.Tests.TestUtils;
+
+namespace ZAProxy.Tests.Components
+{
+    [Trait("Component", "AutoUpdate")]
+    public class AutoUpdateTests
+    {
+        [Theory, AutoTestData]
+        public void ComponentName(
+            [Greedy]AutoUpdate sut)
+        {
+            // ACT
+            var result = sut.ComponentName;
+
+            // ASSERT
+            result.Should().Be("autoupdate");
+        }
+
+        #region Views
+
+        [Theory, AutoTestData]
+        public void IsLatestVersion(
+            [Frozen]Mock<IHttpClient> httpClientMock,
+            [Greedy]AutoUpdate sut,
+            bool isLatestVersion)
+        {
+            // ARRANGE
+            var json = new JObject(
+                new JProperty("isLatestVersion", isLatestVersion));
+            httpClientMock.SetupApiCall(sut, CallType.View, "isLatestVersion")
+                .Returns(json.ToString())
+                .Verifiable();
+
+            // ACT
+            var result = sut.IsLatestVersion();
+
+            // ASSERT
+            result.Should().Be(isLatestVersion);
+            httpClientMock.Verify();
+        }
+
+        [Theory, AutoTestData]
+        public void GetLatestVersionNumber(
+            [Frozen]Mock<IHttpClient> httpClientMock,
+            [Greedy]AutoUpdate sut,
+            string latestVersionNumber)
+        {
+            // ARRANGE
+            var json = new JObject(
+                new JProperty("latestVersionNumber", latestVersionNumber));
+            httpClientMock.SetupApiCall(sut, CallType.View, "latestVersionNumber")
+                .Returns(json.ToString())
+                .Verifiable();
+
+            // ACT
+            var result = sut.GetLatestVersionNumber();
+
+            // ASSERT
+            result.Should().Be(latestVersionNumber);
+            httpClientMock.Verify();
+        }
+
+        [Theory, AutoTestData]
+        public void GetOptionCheckAddonUpdates(
+            [Frozen]Mock<IHttpClient> httpClientMock,
+            [Greedy]AutoUpdate sut,
+            bool value)
+        {
+            // ARRANGE
+            var json = new JObject(
+                new JProperty("CheckAddonUpdates", value));
+            httpClientMock.SetupApiCall(sut, CallType.View, "optionCheckAddonUpdates")
+                .Returns(json.ToString())
+                .Verifiable();
+
+            // ACT
+            var result = sut.GetOptionCheckAddonUpdates();
+
+            // ASSERT
+            result.Should().Be(value);
+            httpClientMock.Verify();
+        }
+
+        [Theory, AutoTestData]
+        public void GetOptionCheckOnStart(
+            [Frozen]Mock<IHttpClient> httpClientMock,
+            [Greedy]AutoUpdate sut,
+            bool value)
+        {
+            // ARRANGE
+            var json = new JObject(
+                new JProperty("CheckOnStart", value));
+            httpClientMock.SetupApiCall(sut, CallType.View, "optionCheckOnStart")
+                .Returns(json.ToString())
+                .Verifiable();
+
+            // ACT
+            var result = sut.GetOptionCheckOnStart();
+
+            // ASSERT
+            result.Should().Be(value);
+            httpClientMock.Verify();
+        }
+
+        [Theory, AutoTestData]
+        public void GetOptionDownloadNewRelease(
+            [Frozen]Mock<IHttpClient> httpClientMock,
+            [Greedy]AutoUpdate sut,
+            bool value)
+        {
+            // ARRANGE
+            var json = new JObject(
+                new JProperty("DownloadNewRelease", value));
+            httpClientMock.SetupApiCall(sut, CallType.View, "optionDownloadNewRelease")
+                .Returns(json.ToString())
+                .Verifiable();
+
+            // ACT
+            var result = sut.GetOptionDownloadNewRelease();
+
+            // ASSERT
+            result.Should().Be(value);
+            httpClientMock.Verify();
+        }
+
+        [Theory, AutoTestData]
+        public void GetOptionInstallAddonUpdates(
+            [Frozen]Mock<IHttpClient> httpClientMock,
+            [Greedy]AutoUpdate sut,
+            bool value)
+        {
+            // ARRANGE
+            var json = new JObject(
+                new JProperty("InstallAddonUpdates", value));
+            httpClientMock.SetupApiCall(sut, CallType.View, "optionInstallAddonUpdates")
+                .Returns(json.ToString())
+                .Verifiable();
+
+            // ACT
+            var result = sut.GetOptionInstallAddonUpdates();
+
+            // ASSERT
+            result.Should().Be(value);
+            httpClientMock.Verify();
+        }
+
+        [Theory, AutoTestData]
+        public void GetOptionInstallScannerRules(
+            [Frozen]Mock<IHttpClient> httpClientMock,
+            [Greedy]AutoUpdate sut,
+            bool value)
+        {
+            // ARRANGE
+            var json = new JObject(
+                new JProperty("InstallScannerRules", value));
+            httpClientMock.SetupApiCall(sut, CallType.View, "optionInstallScannerRules")
+                .Returns(json.ToString())
+                .Verifiable();
+
+            // ACT
+            var result = sut.GetOptionInstallScannerRules();
+
+            // ASSERT
+            result.Should().Be(value);
+            httpClientMock.Verify();
+        }
+
+        [Theory, AutoTestData]
+        public void GetOptionReportAlphaAddons(
+            [Frozen]Mock<IHttpClient> httpClientMock,
+            [Greedy]AutoUpdate sut,
+            bool value)
+        {
+            // ARRANGE
+            var json = new JObject(
+                new JProperty("ReportAlphaAddons", value));
+            httpClientMock.SetupApiCall(sut, CallType.View, "optionReportAlphaAddons")
+                .Returns(json.ToString())
+                .Verifiable();
+
+            // ACT
+            var result = sut.GetOptionReportAlphaAddons();
+
+            // ASSERT
+            result.Should().Be(value);
+            httpClientMock.Verify();
+        }
+
+        [Theory, AutoTestData]
+        public void GetOptionReportBetaAddons(
+            [Frozen]Mock<IHttpClient> httpClientMock,
+            [Greedy]AutoUpdate sut,
+            bool value)
+        {
+            // ARRANGE
+            var json = new JObject(
+                new JProperty("ReportBetaAddons", value));
+            httpClientMock.SetupApiCall(sut, CallType.View, "optionReportBetaAddons")
+                .Returns(json.ToString())
+                .Verifiable();
+
+            // ACT
+            var result = sut.GetOptionReportBetaAddons();
+
+            // ASSERT
+            result.Should().Be(value);
+            httpClientMock.Verify();
+        }
+
+        [Theory, AutoTestData]
+        public void GetOptionReportReleaseAddons(
+            [Frozen]Mock<IHttpClient> httpClientMock,
+            [Greedy]AutoUpdate sut,
+            bool value)
+        {
+            // ARRANGE
+            var json = new JObject(
+                new JProperty("ReportReleaseAddons", value));
+            httpClientMock.SetupApiCall(sut, CallType.View, "optionReportReleaseAddons")
+                .Returns(json.ToString())
+                .Verifiable();
+
+            // ACT
+            var result = sut.GetOptionReportReleaseAddons();
+
+            // ASSERT
+            result.Should().Be(value);
+            httpClientMock.Verify();
+        }
+
+        #endregion
+
+        #region Actions
+
+        [Theory, AutoTestData]
+        public void DownloadLatestRelease(
+            [Frozen]Mock<IHttpClient> httpClientMock,
+            [Greedy]AutoUpdate sut)
+        {
+            // ARRANGE
+            httpClientMock.SetupApiCall(sut, CallType.Action, "downloadLatestRelease")
+                .ReturnsOkResult()
+                .Verifiable();
+
+            // ACT
+            sut.DownloadLatestRelease();
+
+            // ASSERT
+            httpClientMock.Verify();
+        }
+
+        [Theory, AutoTestData]
+        public void SetOptionCheckAddonUpdates(
+            [Frozen]Mock<IHttpClient> httpClientMock,
+            [Greedy]AutoUpdate sut,
+            bool value)
+        {
+            // ARRANGE
+            httpClientMock.SetupApiCall(sut, CallType.Action, "optionCheckAddonUpdates",
+                new Parameters
+                {
+                    { "Boolean", value }
+                })
+                .ReturnsOkResult()
+                .Verifiable();
+
+            // ACT
+            sut.SetOptionCheckAddonUpdates(value);
+
+            // ASSERT
+            httpClientMock.Verify();
+        }
+
+        [Theory, AutoTestData]
+        public void SetOptionCheckOnStart(
+            [Frozen]Mock<IHttpClient> httpClientMock,
+            [Greedy]AutoUpdate sut,
+            bool value)
+        {
+            // ARRANGE
+            httpClientMock.SetupApiCall(sut, CallType.Action, "optionCheckOnStart",
+                new Parameters
+                {
+                    { "Boolean", value }
+                })
+                .ReturnsOkResult()
+                .Verifiable();
+
+            // ACT
+            sut.SetOptionCheckOnStart(value);
+
+            // ASSERT
+            httpClientMock.Verify();
+        }
+
+        [Theory, AutoTestData]
+        public void SetOptionDownloadNewRelease(
+            [Frozen]Mock<IHttpClient> httpClientMock,
+            [Greedy]AutoUpdate sut,
+            bool value)
+        {
+            // ARRANGE
+            httpClientMock.SetupApiCall(sut, CallType.Action, "optionDownloadNewRelease",
+                new Parameters
+                {
+                    { "Boolean", value }
+                })
+                .ReturnsOkResult()
+                .Verifiable();
+
+            // ACT
+            sut.SetOptionDownloadNewRelease(value);
+
+            // ASSERT
+            httpClientMock.Verify();
+        }
+
+        [Theory, AutoTestData]
+        public void SetOptionInstallAddonUpdates(
+            [Frozen]Mock<IHttpClient> httpClientMock,
+            [Greedy]AutoUpdate sut,
+            bool value)
+        {
+            // ARRANGE
+            httpClientMock.SetupApiCall(sut, CallType.Action, "optionInstallAddonUpdates",
+                new Parameters
+                {
+                    { "Boolean", value }
+                })
+                .ReturnsOkResult()
+                .Verifiable();
+
+            // ACT
+            sut.SetOptionInstallAddonUpdates(value);
+
+            // ASSERT
+            httpClientMock.Verify();
+        }
+
+        [Theory, AutoTestData]
+        public void SetOptionInstallScannerRules(
+            [Frozen]Mock<IHttpClient> httpClientMock,
+            [Greedy]AutoUpdate sut,
+            bool value)
+        {
+            // ARRANGE
+            httpClientMock.SetupApiCall(sut, CallType.Action, "optionInstallScannerRules",
+                new Parameters
+                {
+                    { "Boolean", value }
+                })
+                .ReturnsOkResult()
+                .Verifiable();
+
+            // ACT
+            sut.SetOptionInstallScannerRules(value);
+
+            // ASSERT
+            httpClientMock.Verify();
+        }
+
+        [Theory, AutoTestData]
+        public void SetOptionReportAlphaAddons(
+            [Frozen]Mock<IHttpClient> httpClientMock,
+            [Greedy]AutoUpdate sut,
+            bool value)
+        {
+            // ARRANGE
+            httpClientMock.SetupApiCall(sut, CallType.Action, "optionReportAlphaAddons",
+                new Parameters
+                {
+                    { "Boolean", value }
+                })
+                .ReturnsOkResult()
+                .Verifiable();
+
+            // ACT
+            sut.SetOptionReportAlphaAddons(value);
+
+            // ASSERT
+            httpClientMock.Verify();
+        }
+
+        [Theory, AutoTestData]
+        public void SetOptionReportBetaAddons(
+            [Frozen]Mock<IHttpClient> httpClientMock,
+            [Greedy]AutoUpdate sut,
+            bool value)
+        {
+            // ARRANGE
+            httpClientMock.SetupApiCall(sut, CallType.Action, "optionReportBetaAddons",
+                new Parameters
+                {
+                    { "Boolean", value }
+                })
+                .ReturnsOkResult()
+                .Verifiable();
+
+            // ACT
+            sut.SetOptionReportBetaAddons(value);
+
+            // ASSERT
+            httpClientMock.Verify();
+        }
+
+        [Theory, AutoTestData]
+        public void SetOptionReportReleaseAddons(
+            [Frozen]Mock<IHttpClient> httpClientMock,
+            [Greedy]AutoUpdate sut,
+            bool value)
+        {
+            // ARRANGE
+            httpClientMock.SetupApiCall(sut, CallType.Action, "optionReportReleaseAddons",
+                new Parameters
+                {
+                    { "Boolean", value }
+                })
+                .ReturnsOkResult()
+                .Verifiable();
+
+            // ACT
+            sut.SetOptionReportReleaseAddons(value);
+
+            // ASSERT
+            httpClientMock.Verify();
+        }
+
+        #endregion
+    }
+}
---
+++ ZAProxy/Components/AutoUpdate.cs
@@ -0,0 +1,230 @@
+﻿using ZAProxy.Infrastructure;
+
+namespace ZAProxy.Components
+{
+    /// <summary>
+    /// Component that manages the updating of ZAP and it's components.
+    /// </summary>
+    public class AutoUpdate : ComponentBase
+    {
+        /// <summary>
+        /// Initiates a new instance of the <see cref="AutoUpdate"/> class.
+        /// </summary>
+        /// <param name="zapProcess">The ZAP process to connect to.</param>
+        public AutoUpdate(IZapProcess zapProcess)
+            : this(null, zapProcess)
+        { }
+
+        /// <summary>
+        /// Initiates a new instance of the <see cref="AutoUpdate"/> class with a specific HTTP client implementation.
+        /// </summary>
+        /// <param name="httpClient">The HTTP client implementation.</param>
+        /// <param name="zapProcess">The ZAP process to connect to.</param>
+        public AutoUpdate(IHttpClient httpClient, IZapProcess zapProcess)
+            : base(httpClient, zapProcess, "autoupdate")
+        { }
+
+        #region Views
+
+        /// <summary>
+        /// Gets whether the current running version of ZAP is the latest.
+        /// </summary>
+        /// <returns></returns>
+        public bool IsLatestVersion()
+        {
+            return CallView<bool>("isLatestVersion", "isLatestVersion");
+        }
+
+        /// <summary>
+        /// Gets the latest version number of ZAP.
+        /// Can be either a daily build or release number, depending on what type of version of ZAP you are running.
+        /// </summary>
+        /// <returns>Latest version number of ZAP.</returns>
+        public string GetLatestVersionNumber()
+        {
+            return CallView<string>("latestVersionNumber", "latestVersionNumber");
+        }
+
+        /// <summary>
+        /// Gets whether ZAP automatically checks for updates.
+        /// </summary>
+        /// <returns>True if ZAP automatically checks for updates.</returns>
+        public bool GetOptionCheckAddonUpdates()
+        {
+            return CallView<bool>("optionCheckAddonUpdates", "CheckAddonUpdates");
+        }
+
+        /// <summary>
+        /// Gets whether ZAP checks for updates when the application is started.
+        /// </summary>
+        /// <returns>True if ZAP checks for updates when the application is started.</returns>
+        public bool GetOptionCheckOnStart()
+        {
+            return CallView<bool>("optionCheckOnStart", "CheckOnStart");
+        }
+
+        /// <summary>
+        /// Gets whether ZAP automatically downloads new releases.
+        /// </summary>
+        /// <returns>True if ZAP automatically downloads new releases.</returns>
+        public bool GetOptionDownloadNewRelease()
+        {
+            return CallView<bool>("optionDownloadNewRelease", "DownloadNewRelease");
+        }
+
+        /// <summary>
+        /// Gets whether ZAP automatically installs addon updates.
+        /// </summary>
+        /// <returns>True if ZAP automatically installs addon updates.</returns>
+        public bool GetOptionInstallAddonUpdates()
+        {
+            return CallView<bool>("optionInstallAddonUpdates", "InstallAddonUpdates");
+        }
+
+        /// <summary>
+        /// Gets whether ZAP automatically installs updated scanner rules.
+        /// </summary>
+        /// <returns>True if ZAP automatically installs updated scanner rules.</returns>
+        public bool GetOptionInstallScannerRules()
+        {
+            return CallView<bool>("optionInstallScannerRules", "InstallScannerRules");
+        }
+
+        /// <summary>
+        /// Gets whether ZAP reports alpha releases of addons as updates.
+        /// </summary>
+        /// <returns>True if ZAP reports alpha releases of addons as updates.</returns>
+        public bool GetOptionReportAlphaAddons()
+        {
+            return CallView<bool>("optionReportAlphaAddons", "ReportAlphaAddons");
+        }
+
+        /// <summary>
+        /// Gets whether ZAP reports beta releases of addons as updates.
+        /// </summary>
+        /// <returns>True if ZAP reports beta releases of addons as updates.</returns>
+        public bool GetOptionReportBetaAddons()
+        {
+            return CallView<bool>("optionReportBetaAddons", "ReportBetaAddons");
+        }
+
+        /// <summary>
+        /// Gets whether ZAP reports stable releases of addons as updates.
+        /// </summary>
+        /// <returns>True if ZAP reports stable releases of addons as updates.</returns>
+        public bool GetOptionReportReleaseAddons()
+        {
+            return CallView<bool>("optionReportReleaseAddons", "ReportReleaseAddons");
+        }
+
+        #endregion
+
+        #region Actions
+
+        /// <summary>
+        /// Downloads the latest release of ZAP.
+        /// </summary>
+        public void DownloadLatestRelease()
+        {
+            CallAction("downloadLatestRelease");
+        }
+
+        /// <summary>
+        /// Sets whether ZAP should automatically check for addon updates.
+        /// </summary>
+        /// <param name="value">True if ZAP should automatically check for addon updates.</param>
+        public void SetOptionCheckAddonUpdates(bool value)
+        {
+            CallAction("optionCheckAddonUpdates", new Parameters
+            {
+                { "Boolean", value }
+            });
+        }
+
+        /// <summary>
+        /// Sets whether ZAP should check for updates when the application is started.
+        /// </summary>
+        /// <param name="value">True if ZAP should check for updates when the application is started.</param>
+        public void SetOptionCheckOnStart(bool value)
+        {
+            CallAction("optionCheckOnStart", new Parameters
+            {
+                { "Boolean", value }
+            });
+        }
+
+        /// <summary>
+        /// Sets whether ZAP should automatically download new releases.
+        /// </summary>
+        /// <param name="value">True if ZAP should automatically download new releases.</param>
+        public void SetOptionDownloadNewRelease(bool value)
+        {
+            CallAction("optionDownloadNewRelease", new Parameters
+            {
+                { "Boolean", value }
+            });
+        }
+
+        /// <summary>
+        /// Sets whether ZAP should automatically install addon updates.
+        /// </summary>
+        /// <param name="value">True if ZAP should automatically install addon updates.</param>
+        public void SetOptionInstallAddonUpdates(bool value)
+        {
+            CallAction("optionInstallAddonUpdates", new Parameters
+            {
+                { "Boolean", value }
+            });
+        }
+
+        /// <summary>
+        /// Sets whether ZAP should automatically install updates to scanner rules.
+        /// </summary>
+        /// <param name="value">True if ZAP should automatically install updates to scanner rules.</param>
+        public void SetOptionInstallScannerRules(bool value)
+        {
+            CallAction("optionInstallScannerRules", new Parameters
+            {
+                { "Boolean", value }
+            });
+        }
+
+        /// <summary>
+        /// Sets whether ZAP should report alpha releases of addons as updates.
+        /// </summary>
+        /// <param name="value">True if ZAP should report alpha releases of addons as updates.</param>
+        public void SetOptionReportAlphaAddons(bool value)
+        {
+            CallAction("optionReportAlphaAddons", new Parameters
+            {
+                { "Boolean", value }
+            });
+        }
+
+        /// <summary>
+        /// Sets whether ZAP should report beta releases of addons as updates.
+        /// </summary>
+        /// <param name="value">True if ZAP should report beta releases of addons as updates.</param>
+        public void SetOptionReportBetaAddons(bool value)
+        {
+            CallAction("optionReportBetaAddons", new Parameters
+            {
+                { "Boolean", value }
+            });
+        }
+
+        /// <summary>
+        /// Sets whether ZAP should report stable releases of addons as updates.
+        /// </summary>
+        /// <param name="value">True if ZAP should report stable releases of addons as updates.</param>
+        public void SetOptionReportReleaseAddons(bool value)
+        {
+            CallAction("optionReportReleaseAddons", new Parameters
+            {
+                { "Boolean", value }
+            });
+        }
+
+        #endregion
+    }
+}
